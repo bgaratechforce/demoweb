8fd649d58ef6b703cc9b7cdb810d98a8f005b50f
sivasirish <sivasirish@gmail.com>
2017-07-12 04:53:23 -0400

    first commit

---
+++ src/main/java/com/speedops/demoapp/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.speedops.demoapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DemoController {  
+    @RequestMapping("/welcome")  
+    public ModelAndView welcome() {  
+        String message = "Welcome to Speedops";  
+        return new ModelAndView("hellopage", "message", message);  
+    }  
+}  
---
+++ src/main/webapp/WEB-INF/jsp/hellopage.jsp
@@ -0,0 +1,10 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false" %>
+<html>
+<body>
+	<h1>Speedops Sample Application</h1>
+
+	<h2>${message}</h2>
+
+</body>
+</html>
