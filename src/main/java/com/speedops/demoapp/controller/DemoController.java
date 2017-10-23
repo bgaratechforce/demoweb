14ff99f0ea9cf9324e4ec36bb6ec38b9cd4c723f
sivasirish <sivasirish@gmail.com>
2017-10-23 15:03:26 -0400

    Update DemoController.java

--- src/main/java/com/speedops/demoapp/controller/DemoController.java
+++ src/main/java/com/speedops/demoapp/controller/DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class DemoController {  
     @RequestMapping("/welcome")  
     public ModelAndView welcome() {  
-        String message = "Welcome team to Speedops";  
+        String message = "Welcome team to Techforce.ai";  
         return new ModelAndView("hellopage", "message", message);  
     }  
 }  
